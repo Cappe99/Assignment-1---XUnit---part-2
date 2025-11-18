11b92c0eb35fed3ba63503031f73d7974e02b1e8
Casper Björlin <cb224cd@student.lnu.se>
2025-11-18 13:51:35 +0100

    RED, AnswerOption Test

---
+++ app/src/test/java/ass1/AnswerOptionTest.java
@@ -0,0 +1,14 @@
+package ass1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnswerOptionTest {
+
+    @Test
+    void shouldReturnTrueIfOptionIsCorrect() {
+        AnswerOption opt = new AnswerOption("4", true);
+        assertTrue(opt.isCorrect());
+    }
+
+}
