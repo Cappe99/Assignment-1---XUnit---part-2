16c8f9a60ff38e14126a3ecb39f7bbc08d0ca035
Casper Björlin <cb224cd@student.lnu.se>
2025-11-18 14:02:34 +0100

    RED, QuizController, run and update score

---
+++ app/src/test/java/ass1/QuizControllerTest.java
@@ -0,0 +1,20 @@
+package ass1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+public class QuizControllerTest {
+    @Test
+    void shouldRunQuizAndUpdateScore() {
+        Quiz quiz = new Quiz();
+        Question question = new Question("What is 2+2?", List.of("3", "4", "5"), 1);
+        quiz.addQuestion(question);
+
+        QuizController controller = new QuizController(quiz);
+
+        controller.answerQuestion(question, 1);
+
+        assertEquals(1, quiz.getScore());
+    }
+}
